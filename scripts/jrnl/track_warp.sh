226a5998ecbd5b10008e7598078be8bd33e34734
Raphael Reme <raphaelreme@protonmail.com>
2025-09-30 16:40:17 +0200

    Update WTT

--- scripts/jrnl/track_warp.sh
+++ scripts/jrnl/track_warp.sh
@@ -4,35 +4,54 @@ set -e
 
 SCRIPTPATH=$(dirname "$0")
 
-# Fixed detla = 50, PSNR = 1.5. Different detections method ?
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 50.0 fake 0.0 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 50.0 fake 0.1 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 50.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 50.0 fake 0.3 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 50.0 fake 0.4 $@
+# Different detections method ?
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.2 50.0 fake 0.0 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.2 50.0 fake 0.1 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.2 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.2 50.0 fake 0.3 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.2 50.0 fake 0.4 $@
 
-# Wavelet on springs/flows
-bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 50.0 wavelet 0.2 $@
-bash $SCRIPTPATH/_track_warp.sh springs 1.5 50.0 wavelet 0.2 $@
+# Wavelet on springs/flow
+bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.2 50.0 wavelet 0.2 $@
+bash $SCRIPTPATH/_track_warp.sh springs_2d 0.2 50.0 wavelet 0.2 $@
 
 
-# HOTA vs PSNR on flows (Fake@80%)
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 0.1 5.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 0.5 5.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 5.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 3.0 5.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 10.0 5.0 fake 0.2 $@
+# Noise impact on Hydra Flow (Fake@80%)
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.02 5.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.07 5.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.20 5.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.35 5.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.85 5.0 fake 0.2 $@
 
-bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 0.1 50.0 fake 0.2 $@
-bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 0.5 50.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 50.0 fake 0.2 $@
-bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 3.0 50.0 fake 0.2 $@
-bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 10.0 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.02 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.07 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.20 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.35 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.85 50.0 fake 0.2 $@
 
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 0.1 50.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 0.5 50.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 1.5 50.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 3.0 50.0 fake 0.2 $@
-# bash $SCRIPTPATH/_track_warp.sh flow_20140829_1 10.0 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.02 500.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.07 500.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.20 500.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.35 500.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh hydra_flow 0.85 500.0 fake 0.2 $@
 
+
+# Noise impact on Springs 2D (Fake@80%)
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.02 5.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.07 5.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.20 5.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.35 5.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.85 5.0 fake 0.2 $@
+
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.02 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.07 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.20 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.35 50.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.85 50.0 fake 0.2 $@
+
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.02 500.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.07 500.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.20 500.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.35 500.0 fake 0.2 $@
+# bash $SCRIPTPATH/_track_warp.sh springs_2d 0.85 500.0 fake 0.2 $@
 
